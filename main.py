c4af63305cb5bad741ffcfbe9825d1cfdc7d7f09
hiago1220s <hiagopereira38@gmail.com>
2026-02-10 06:18:52 -0300

    creating main loop conecting menu and game classes

--- main.py
+++ main.py
@@ -1,18 +1,4 @@
-import pygame
+from code.game import Game
 
-print('setup da janela comeca')
-pygame.init()
-window = pygame.display.set_mode(size=(600, 480))
-print('janela fecha')
-
-print('looping comeca')
-while True:
-    # check for all events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit() # Closse window
-            quit() # end pygame
-
-
-
-
+game = Game()
+game.run()
